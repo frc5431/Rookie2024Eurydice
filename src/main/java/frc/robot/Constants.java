21599e2c8011692b1c31f43d0b69f670eb406447
micw1417 <wu355049@gmail.com>
2024-05-02 17:27:37 -0500

    Got the motor IDs for drivebase

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -5,18 +5,17 @@
 package frc.robot;
 public final class Constants {
   public static class ManipulatorConstants {
-    public static final int leftManipulatorID = 0; // temp
-    public static final int rightManipulatorID = 1; // temp
+    public static final int leftManipulatorID = 555; // temp
+    public static final int rightManipulatorID = 555; // temp
 
     public static final double intakeSpeed = -1; // temp
     public static final double shooterSpeed = -1; // temp
   }
 
   public static class DrivebaseConstants {
-    public static final int leftBackID = 2; // temp
-    public static final int leftFrontID = 3; // temp
-    public static final int rightBackID = 4; // temp
-    public static final int rightfrontID = 5; // temp
-    
+    public static final int leftBackID = 1;
+    public static final int leftFrontID = 2;
+    public static final int rightBackID = 4;
+    public static final int rightfrontID = 3;
   }
 }
